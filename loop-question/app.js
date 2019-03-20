157653537b3e52097e55b5b8818191c186ac87c1
Jomar Mendoza <Omay@Macintosh.local>
2019-03-19 18:11:59 -0700

    Commit 9 - Created loops.js file

---
+++ loop-question/app.js
@@ -0,0 +1,36 @@
+// for(let i = 0; i <= 10; i++){
+//   console.log(i)
+// }
+//
+// for (let i = 10; i >= 0; i--){
+//   console.log(i)
+// }
+
+// for(let i = 298; i >= 1; i--){
+//   console.log(i)
+//    i = i - 2;
+// }
+
+// for (let i = 0; i <= 98; i++){
+//   if (i % 2 == 0) {
+//     console.log(i)
+//   }
+// }
+
+// for(let i =98; i>=0; i--){
+//   if(i % 2 != 0){
+//     console.log(i)
+//   }
+// }
+
+// for(let i = 49; i <= 72; i++){
+//   console.log(i)
+// }
+
+// for(let i = 81; i >= 26; i--){
+//   console.log(i)
+// }
+
+// for(let i = 1; i <= 91; i++){
+//   console.log(i * 3)
+// }
